1ca359c6d2d9a5da70990bf33db0b2530729f1fe
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-10-29 14:38:41 -0300

    315

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# ttdne
+# THIS THING DO NOT EXIST
+
+Live view on https://jmttdne.netlify.app/
+
+<img src="01.png">
+
+<img src="02.png">
+
+<img src="03.png">
---
+++ index.js
@@ -0,0 +1,8 @@
+let cat = document.getElementById('cat-01')
+
+let btnReload = document.getElementById('reload-img')
+
+btnReload.onclick = () => {
+    window.location('reload')
+    console.log('hola')
+}
