d70db6bc1329e81a8a0a04f15c7ee8d0b78de0f7
Joaquin Metayer <joaquinmetayer@gmail.com>
2022-10-29 14:43:38 -0300

    325

--- index.js
+++
@@ -1,8 +0,0 @@
-let cat = document.getElementById('cat-01')
-
-let btnReload = document.getElementById('reload-img')
-
-btnReload.onclick = () => {
-    window.location('reload')
-    console.log('hola')
-}
